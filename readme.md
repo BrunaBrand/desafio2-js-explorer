cb9900964f231c0ea3831fd661bdd022d31744b7
Bruna Brandão <bruna.costab@hotmail.com>
2022-07-12 18:00:25 -0300

    final do desafio

---
+++ readme.md
@@ -0,0 +1,3 @@
+Este é um desafio do nível 04 do explorer
+
+- O objetivo é criar uma lista de estudantes com nome e duas notas de prova, criar uma função para calcular a média desses estudantes, e mostrar qual estudante foi aprovado ou reprovado.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Aqui estou criando a variável para colocar os objetos dentro da estrutura de dados, cada {} é um objeto com dados
+//Aqui estou criando a variável para colocar os objetos dentro de uma lista para criar estrutura de dados, cada {} é um objeto com dados. Criei uma const com uma lista de objetos dentro
 const students = [
   {
     name: 'Renata',
@@ -16,22 +16,54 @@ const students = [
     noteProofTwo: 9
   }
 ]
-//Aqui estou criando uma lista vazia que vai receber o nome dos estudandes para mostrar na tela
-let studentsNames = []
-let studentsNote = []
+//Aqui estou criando listas vazias que vai receber o nome dos estudandes para mostrar na tela
+// let studentsNames = []
+// let studentsNote = []
 
-//aqui estou percorrendo a lista de objetos para selecionar apenas o nome dos estudantes
-for (let student of students) {
-  studentsNames.push(student.name)
-}
+// //aqui estou percorrendo a lista de objetos para selecionar apenas o nome dos estudantes
 
-for (let note of students) {
-  studentsNote.push(note.noteProofOne)
-}
+// for (let student of students) {
+//   studentsNames.push(student.name)
+// }
 
 //aqui estou mostrando em tela os nomes dos estudantes criados na lista.
-alert(`
+// alert(`
+//   ${studentsNames}
+// `)
 
-O(a) Estudante ${students[0].name} teve a nota ${students[0].noteProofOne}
+//declarando a função
 
-`)
+function printAverage(students) {
+  let studentNote = (students.noteProofOne + students.noteProofTwo) / 2
+  let average = 7
+
+  let txtStorage = `A média de ${students.name} foi de: ${studentNote.toFixed(
+    2
+  )}`
+
+  if (studentNote >= average) {
+    return `${txtStorage} Parabéns, você foi aprovado!`
+  } else {
+    return `${txtStorage} Não foi dessa vez!`
+  }
+
+  // return `
+  //   A média de ${students.name} foi de: ${(
+  //   (students.noteProofOne + students.noteProofTwo) /
+  //   2
+  // ).toFixed(2)}
+
+  // `
+}
+
+// if (printAverage >= 7) {
+//   ;(' Parabéns, você foi aprovado(a) no consurso')
+// } else {
+//   ;(' Não foi dessa vez, tente na próxima')
+// }
+
+for (let student of students) {
+  let averageMessage = printAverage(student)
+
+  alert(averageMessage)
+}
